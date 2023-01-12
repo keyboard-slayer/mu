c2f516930338b03a54c625674e29bf7ade27105e
Chrapati <35665560+chrapati24@users.noreply.github.com>
2023-01-12 18:33:48 +0100

    fix(debug): fixed debug for windows

--- src/libs/macro.h
+++ src/libs/macro.h
@@ -6,8 +6,7 @@
 #define packed __attribute__((packed))
 
 #if defined(HOST_IS_WINDOWS)
-    #define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+#    define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
 #else
-    #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+#    define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 #endif
-
